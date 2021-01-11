7d0f3d8b564aabf6ea3532aef23478661a67ead0
prasad091 <prasad.thangavel@gmail.com>
2021-01-11 19:08:59 +0530

    variables and declaration

---
+++ src/main/kotlin/variabledeclaration/VariablesKotlin.kt
@@ -0,0 +1,28 @@
+package variabledeclaration
+
+import kotlin.properties.Delegates
+
+
+var number1:Int = 0
+var number2:Int? = null
+lateinit var number3: String // late init must be initialise
+var number4 by Delegates.notNull<Int>() // Property number4 should be initialized before get
+
+val number5:Int = 1
+const val number6:Int = 1
+
+val number7:Int by lazy { 5 }
+
+
+
+fun main(){
+    number3 = "Hello"
+    number4 = 0
+    println(number1)
+    println(number2)
+    println(number3)
+    println(number4)
+    println(number5)
+    println(number6)
+    println(number7)
+}
