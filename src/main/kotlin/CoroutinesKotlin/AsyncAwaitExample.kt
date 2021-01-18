245c79b3f894276c5717fc7e25d68e578b9ffc4e
prasad091 <prasad.thangavel@gmail.com>
2021-01-18 09:22:54 +0530

    kotlin coroutines Async Await  (#8)

---
+++ src/main/kotlin/CoroutinesKotlin/AsyncAwaitExample.kt
@@ -0,0 +1,17 @@
+package CoroutinesKotlin
+
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+
+suspend fun main(){
+    val one = GlobalScope.async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+    val await = one.await()
+    println(await)
+}
+
+suspend fun doSomethingUsefulOne(): Int {
+    delay(1000L) // pretend we are doing something useful here
+    return 13
+}
