d881a7447bd8fbe6ea9cd6029bcda9c51b45fec3
prasad091 <prasad.thangavel@gmail.com>
2021-01-23 13:11:22 +0530

    Higher order function (#15)

---
+++ src/main/kotlin/HigherOrderFunction/Operator.kt
@@ -0,0 +1,14 @@
+package HigherOrderFunction
+
+class Operator {
+    fun add(a: Int, b: Int) = a + b
+    fun inc(a: Int) = a + 1
+}
+
+fun calc(a: Int, b: Int, opr: (Int, Int) -> Int) = opr(a, b)
+fun calc(a: Int, opr: (Int) -> Int) = opr(a)
+
+fun main() {
+    calc(1, 2) { a, b -> Operator().add(a, b) }
+    calc(1) { Operator().inc(it) }
+}
